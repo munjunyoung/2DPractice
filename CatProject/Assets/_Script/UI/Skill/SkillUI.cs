a41de1628dd58c1171e257fdf895474695f3ef81
unknown <sswsswsswss@gmail.com>
2018-11-01 21:46:30 +0900

    우측 하단 스킬 ui 추가 및 조이스틱 ui 변경

---
+++ CatProject/Assets/_Script/UI/Skill/SkillUI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SkillUI : MonoBehaviour {
+
+    private bool onClick;
+    public RectTransform skillLinePanel;
+    private Animator skillLineAnim;
+    
+    public List<RectTransform> skillList;
+	// Use this for initialization
+	void Start () {
+        onClick = false;
+        skillLineAnim = skillLinePanel.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ClickSkillMenuButton()
+    {
+        onClick = onClick ? false : true;
+        skillLineAnim.SetBool("onClick", onClick);
+    }
+
+    public void ClickSkill1Button()
+    {
+
+    }
+
+    public void ClickSkill2Button()
+    {
+
+    }
+
+    public void ClickSkill3Button()
+    {
+
+    }
+    
+}
